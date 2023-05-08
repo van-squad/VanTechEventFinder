7467b7e9920b60f0a5e2824c461558d9e9cdab6e
Joy Lee <joylee.dev@gmail.com>
2023-05-08 00:16:36 -0700

    style : add a link to the MAP page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,24 @@
 import { type NextPage } from "next";
 import { SEO } from "~/components/SEO";
 import { Button, Text } from "@mantine/core";
+import Link from "next/link";
 
 const Home: NextPage = () => {
   return (
     <>
       <SEO title="Intro" />
       <main className="flex h-screen flex-col items-center justify-center">
-        <Text fz="lg" fw={700} mb={20}>
+        <Text fz="lg" fw={700} mb={10}>
           Tech Meetup Map 👋
         </Text>
-        <Button>Find Events</Button>
+        <Text fw={500} mb={20} maw={380} ta="center">
+          Looking for a way to stay up-to-date on the latest tech meetups
+          happening in Vancouver? This app displays all upcoming tech events in
+          Vancouver, and you can search for events by date.
+        </Text>
+        <Link href={`/map`}>
+          <Button>Find Events</Button>
+        </Link>
       </main>
     </>
   );
