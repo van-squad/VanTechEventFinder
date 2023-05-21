60c8b60059ca3861aa8a60df19fc03265f8e3656
KEENTO <handball0321@gmail.com>
2023-05-22 06:10:04 +0900

    Reset settings for husky for some errors

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -15,7 +15,9 @@ const config = {
     },
   ],
   parser: "@typescript-eslint/parser",
-  parserOptions: {},
+  parserOptions: {
+    // project: path.join(__dirname, "tsconfig.json"),
+  },
   plugins: ["@typescript-eslint"],
   extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
   rules: {
