1298e65f3b374a94bccf026d2eee7632772e0d23
Joy Lee <joylee.dev@gmail.com>
2023-06-11 11:26:00 -0700

    style: add global theme and custom theme

--- src/clients/contexts/MantineProvider.tsx
+++ src/clients/contexts/MantineProvider.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import {
+  Global,
   MantineProvider as Mantine,
   useMantineColorScheme,
 } from "@mantine/core";
+import theme from "~/utils/theme";
 
 export interface MantineProviderProps {
   children: React.ReactNode;
@@ -13,7 +15,16 @@ const MantineProvider = ({ children }: MantineProviderProps) => {
   const { colorScheme } = useMantineColorScheme();
 
   return (
-    <Mantine theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
+    <Mantine theme={theme} withGlobalStyles withNormalizeCSS>
+      <Global
+        styles={(theme) => ({
+          body: {
+            backgroundColor:
+              colorScheme === "dark" ? theme.colors.blue[3] : theme.white,
+            color: colorScheme === "dark" ? theme.colors.dark[0] : theme.black,
+          },
+        })}
+      />
       {children}
     </Mantine>
   );
