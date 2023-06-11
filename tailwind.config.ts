024cd5ea8a372ca5e157af693c674416f5910938
Lada496 <67321065+Lada496@users.noreply.github.com>
2023-06-10 21:23:19 -0700

    set up custome colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,15 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      navy: {
+        100: "#e0fbfc",
+        200: "#98c1d9",
+        300: "#3d5a80",
+        400: "#293241",
+      },
+      salmon: "#ee6c4d",
+    },
   },
   plugins: [],
   darkMode: "class",
