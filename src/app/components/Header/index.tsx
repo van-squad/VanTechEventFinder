d559bc8355bdd457b6f7b984e5ad958c3e6bbf9f
Sachi Goto <70562492+SachiGoto@users.noreply.github.com>
2023-08-03 16:19:25 -0700

    pushed index file from the header component

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -14,10 +14,35 @@ import {
 import { useDisclosure } from "@mantine/hooks";
 import { ThemeSwitch } from "../../components";
 import { useStyles, HEADER_HEIGHT } from "./styles";
+import { BUTTON_VARIANTS } from "~/components/Button";
+import { Button } from "~/components";
+
+const loggedIn = false; 
 
 const LINKS = [
-  { link: "/", label: "Home" },
-  { link: "/map", label: "Map" },
+  { link: "/map", label: "Map", buttonType: BUTTON_VARIANTS.PRIMARY },
+  loggedIn
+    ? {
+        link: "/favlist",
+        label: "Fav Events",
+        buttonType: BUTTON_VARIANTS.SECONDARY,
+      }
+    : {
+        link: "/login",
+        label: "Login",
+        buttonType: BUTTON_VARIANTS.SECONDARY,
+      },
+  loggedIn
+    ? {
+        link: "/logout",
+        label: "Log Out",
+        buttonType: BUTTON_VARIANTS.TERTIARY,
+      }
+    : {
+        link: "/signup",
+        label: "Sign Up",
+        buttonType: BUTTON_VARIANTS.TERTIARY,
+      },
 ];
 
 const Header = () => {
@@ -25,42 +50,58 @@ const Header = () => {
   const [opened, { toggle }] = useDisclosure(false);
   const { classes, cx } = useStyles();
 
-  const items = LINKS.map((link) => (
-    <Link
-      key={link.label}
-      href={link.link}
-      className={cx(classes.link, {
-        [classes.linkActive]: activePathname === link.link,
-      })}
-    >
-      {link.label}
-    </Link>
-  ));
+  const items = LINKS.map((link) =>
+    opened ? (
+      <Link
+        key={link.label}
+        href={link.link}
+        className={cx(classes.link, {
+          [classes.linkActive]: activePathname === link.link,
+        })}
+      >
+        {link.label}
+      </Link>
+    ) : (
+      <Link
+        key={link.label}
+        href={link.link}
+        className={cx(classes.link, {
+          [classes.linkActive]: activePathname === link.link,
+        })}
+      >
+        <Button buttonType={link.buttonType} style={{ marginTop: "5%" }}>
+          {link.label}{" "}
+        </Button>
+      </Link>
+    )
+  );
 
   return (
     <MantineHeader height={HEADER_HEIGHT} className={classes.root}>
       <Container className={classes.header}>
-        <div>
-          <Image src="/images/logo.png" alt="logo" width={45.5} height={57.5} />{" "}
-          Tech Meets
-        </div>
-        <Group spacing={5} className={classes.links}>
-          {items}
-          <ThemeSwitch />
-        </Group>
+        <Link href="/">
+          <Image src="/images/logo.svg" alt="logo" width={45.5} height={57.5} />
+        </Link>
 
-        <Burger
-          opened={opened}
-          onClick={toggle}
-          className={classes.burger}
-          size="sm"
-        />
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <ThemeSwitch />
+
+          <Group spacing={5} className={classes.links}>
+            {items}
+          </Group>
+
+          <Burger
+            opened={opened}
+            onClick={toggle}
+            className={classes.burger}
+            size="sm"
+          />
+        </div>
 
         <Transition transition="pop-top-right" duration={200} mounted={opened}>
           {(styles) => (
             <Paper className={classes.dropdown} withBorder style={styles}>
               {items}
-              <ThemeSwitch />
             </Paper>
           )}
         </Transition>
