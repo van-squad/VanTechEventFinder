496b0e81c5d2bf9b0fe498963c7f954803410e00
KEENTO <handball0321@gmail.com>
2023-08-06 13:24:26 +0900

    Moved calendar component and added it into GoogleMap component

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,17 +1,13 @@
 "use client";
 
 import { useState } from "react";
-import Calendar from "./components/Calendar";
-import { useStyles } from "./styles";
+import Calendar from "../map/components/Calendar";
+import { useStyles } from "../map/components/GoogleMaps/styles";
 
 const CalendarPage = () => {
   const { classes } = useStyles();
   const [date, setDate] = useState<Date | null>(new Date(Date.now()));
-  return (
-    <div className={classes.container}>
-      <Calendar date={date} setDate={setDate} />
-    </div>
-  );
+  return <Calendar date={date} setDate={setDate} />;
 };
 
 export default CalendarPage;
