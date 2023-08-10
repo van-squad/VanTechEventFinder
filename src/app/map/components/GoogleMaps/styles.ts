fddda7ff29ecf2c0f139711ebe9399fdee80eec1
Joy Lee <joylee.dev@gmail.com>
2023-08-09 18:47:39 -0700

    Fix overflowed height

--- src/app/map/components/GoogleMaps/styles.ts
+++ src/app/map/components/GoogleMaps/styles.ts
@@ -13,8 +13,8 @@ export const useStyles = createStyles((theme) => ({
       theme.colorScheme === "dark" ? theme.colors.blue[3] : theme.white,
   },
   googleMap: {
-    height: "100vh",
     width: "100%",
+    height: "calc(100vh - 4.375rem)",
   },
   cards: { position: "absolute", top: "10px", left: "300px" },
 }));
