3fb2294d8aecbcd869e1613d4d20fb8874fe9e81
KEENTO <handball0321@gmail.com>
2023-09-25 07:54:57 +0900

    Added basic logic of manipulation of fav events

---
+++ src/server/api/routers/favoriteEvent.ts
@@ -0,0 +1,51 @@
+// import { TRPCError } from "@trpc/server";
+import { z } from "zod";
+import { prisma } from "~/server/db";
+import { router, privateProcedure } from "../trpc";
+
+export const favoriteEventsRouter = router({
+  getFavorites: privateProcedure
+    .input(
+      z.object({
+        // userId
+        userId: z.string().nonempty(),
+      })
+    )
+    .query(async ({ input }) => {
+      const favEvents = await prisma.favEvent.findMany({
+        where: { userId: input.userId },
+      });
+      console.log({ favEvents });
+      return favEvents;
+    }),
+  addFavorite: privateProcedure
+    .input(
+      z.object({
+        // meetup data
+        id: z.string().nonempty(),
+        userId: z.string().nonempty(),
+        date: z.date().optional(),
+      })
+    )
+    .mutation(async ({ input }) => {
+      // add data to db through prisma
+      await prisma.favEvent.create({
+        data: {
+          ...input,
+        },
+      });
+    }),
+  deleteFavorite: privateProcedure
+    .input(
+      z.object({
+        // meetup data, userId?
+        id: z.string().nonempty(),
+      })
+    )
+    .mutation(async ({ input }) => {
+      // delete meetup data from favorite list
+      await prisma.favEvent.delete({
+        where: { id: input.id },
+      });
+    }),
+});
