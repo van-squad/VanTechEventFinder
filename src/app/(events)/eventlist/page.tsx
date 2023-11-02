f8302e8e12d8f615f3d7999ba10b796df7ee8ade
Sachi Goto <70562492+SachiGoto@users.noreply.github.com>
2023-11-01 18:04:47 -0700

    added heart and trash icons

--- src/app/(events)/eventlist/page.tsx
+++ src/app/(events)/eventlist/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import { Text } from "@mantine/core";
 import { useStyles } from "./styles";
 import Calendar from "../components/Calendar";
@@ -7,6 +7,26 @@ import EventItem from "./compoments/EventItem";
 import { Button, BUTTON_VARIANTS } from "~/app/components";
 import useFetchEvent from "~/hooks/useFetchEvent";
 import { type ModifiedResult } from "~/app/api/events/all/route";
+import { trpc } from "~/providers";
+import { useSession } from "next-auth/react";
+import { convertLocaleTimeString } from "~/utils/date-converter";
+
+export interface EventInterface {
+  id: string;
+  title: string;
+  eventUrl: string;
+  description: string;
+  venue?: {
+    id?: string;
+    name?: string;
+    address?: string;
+    lat?: number;
+    lng?: number;
+  } | null;
+  imageUrl: string;
+  imageId: string;
+  dateTime: string;
+}
 
 const EventListPage = () => {
   const { classes } = useStyles();
@@ -24,6 +44,24 @@ const EventListPage = () => {
   });
   const hasResult = !loading && !error && result && result?.length > 0;
   const noResult = !loading && !error && result?.length === 0;
+
+   const { data: session } = useSession();
+
+   const { mutate } = trpc.favoriteEvents.addFavorite.useMutation();
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const handleAddFavEvent: (event: EventInterface) => void = useCallback(
+      (event) => {
+        const convertedDate = convertLocaleTimeString(event.dateTime);
+
+        mutate({
+          id: event.id,
+          userId: session?.user.id as string,
+          date: convertedDate,
+        });
+      },
+      [mutate, session?.user.id]
+    );
 
   return (
     <div className={classes.container}>
@@ -52,7 +90,13 @@ const EventListPage = () => {
       )}
 
       {hasResult &&
-        result.map((event) => <EventItem key={event.id} event={event} />)}
+        result.map((event) => (
+          <EventItem
+            key={event.id}
+            event={event}
+            onClick={() => handleAddFavEvent(event)}
+          />
+        ))}
     </div>
   );
 };
